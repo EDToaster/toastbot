ae14fd78c07327aabcc4d7f4cc5a0c16fd73832e
EDToaster <howard3061@gmail.com>
2021-10-01 21:14:54 -0700

    Added run steps

---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /home/ec2-user/server
+sudo /user/bin/java -jar *.jar &
---
+++ scripts/validate.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Waiting 15 seconds before checking health"
+sleep 15
+
+exit 0
