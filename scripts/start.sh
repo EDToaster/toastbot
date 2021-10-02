e060997c23cb619640bb173caea101511d20304a
EDToaster <howard3061@gmail.com>
2021-10-02 01:08:34 -0700

    Close stdout and print

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/server
-sudo DISCORD_TOKEN=$(cat DISCORD_SECRET) /usr/bin/java -jar target/*.jar /tmp/out1 2> /tmp/out2 < /tmp/out3  &
+sudo DISCORD_TOKEN=$(cat DISCORD_SECRET) /usr/bin/java -jar target/*.jar /tmp/out1 2> /tmp/out2 < /dev/null  &
