da5ab98e732dadad52349e0554780662a8108f56
EDToaster <howard3061@gmail.com>
2021-10-02 00:57:12 -0700

    Fix stuff

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/server
-export DISCORD_TOKEN=$(cat DISCORD_SECRET)
-sudo /usr/bin/java -jar target/*.jar &
+sudo DISCORD_TOKEN=$(cat DISCORD_SECRET) /usr/bin/java -jar target/*.jar &
