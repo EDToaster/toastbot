97cec01e8bb35d2d7ef29e59becc60be4937be42
EDToaster <howard3061@gmail.com>
2021-10-02 02:36:37 -0700

    dev null all this shit

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/server
-sudo DISCORD_TOKEN=$(cat DISCORD_SECRET) /usr/bin/java -jar target/*.jar /dev/null 2> /tmp/out2 < /dev/null  &
+sudo DISCORD_TOKEN=$(cat DISCORD_SECRET) /usr/bin/java -jar target/*.jar /dev/null 2> /dev/null < /dev/null  &
