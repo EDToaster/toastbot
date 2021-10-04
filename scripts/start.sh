8248ff50950031296d538962edbd66aa9d4a7e6f
EDToaster <howard3061@gmail.com>
2021-10-03 18:00:27 -0700

    Split secrets into prod/beta

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/server
-DISCORD_TOKEN=$(cat DISCORD_SECRET) nohup /usr/bin/java -jar target/*.jar > /dev/null 2>&1 &
+echo "DISCORD_SECRET_$DEPLOYMENT_GROUP_NAME"
+DISCORD_TOKEN=$(cat "DISCORD_SECRET_$DEPLOYMENT_GROUP_NAME") nohup /usr/bin/java -jar target/*.jar > /dev/null 2>&1 &
 
 exit 0
